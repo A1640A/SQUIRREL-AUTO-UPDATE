0f5134157d48d7385dc9272e161f3fd4bc0c308e
omer dursun <domer4621@gmail.com>
2024-08-16 21:17:49 +0300

    Proje dosyası ekle.

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using Squirrel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace SQUIRREL_AUTO_UPDATE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        void SetVersionTitle()
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var versionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
+            this.Text += $" v.{versionInfo.FileVersion}";
+        }
+
+        async void CheckForUpdates()
+        {
+            try
+            {
+                using (var mgr=await UpdateManager.GitHubUpdateManager("github"))
+                {
+                    var release = await mgr.UpdateApp();
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show($"Failed to check for updates. \n\n {exc.Message}");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SetVersionTitle();
+        }
+    }
+}
